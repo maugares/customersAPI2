647ba7eac9ab4da6f8c7b74550fb994af418c229
maugares <mauro.garciaesteban@gmail.com>
2019-04-17 12:36:15 +0200

    relate customer to company and added one entry for each to test it

--- customers/model.js
+++ customers/model.js
@@ -1,37 +1,47 @@
 const Sequelize = require('sequelize')
 const sequelize = require('../db')
+const Company = require('../companies/model')
 
-const Customer = sequelize.define('customers', {
-  firstName: {
-    type: Sequelize.STRING,
-    field: 'first_name',
-    allowNull: false
+const Customer = sequelize.define(
+  'customers',
+  {
+    firstName: {
+      type: Sequelize.STRING,
+      field: 'first_name',
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      field: 'last_name',
+      allowNull: false
+    },
+    bio: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    addressLine1: {
+      type: Sequelize.STRING,
+      field: 'address_line_1'
+    },
+    addressLine2: {
+      type: Sequelize.STRING,
+      field: 'address_line_2'
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    companyId: {
+      type: Sequelize.INTEGER,
+      field: 'company_id'
+    }
   },
-  lastName: {
-    type: Sequelize.STRING,
-    field: 'last_name',
-    allowNull: false
-  },
-  bio: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  addressLine1: {
-    type: Sequelize.STRING,
-    field: 'address_line_1'
-  },
-  addressLine2: {
-    type: Sequelize.STRING,
-    field: 'address_line_2'
-  },
-  city: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
-}, {
-  timestamps: false,
-  logging: false,
-  tableName: 'customers'
-})
+  {
+    timestamps: false,
+    logging: false,
+    tableName: 'customers'
+  })
+
+Customer.belongsTo(Company)
 
 module.exports = Customer
